50f307f23cb27b2294196cdd25890764ea22924e
Jean Moraes <moraes.jean91@gmail.com>
2023-01-13 10:51:58 -0300

    feat: adicionando novas fontes ao projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      highlight: ['"Crimson Pro"', "serif"],
+      main: ["Lato", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
